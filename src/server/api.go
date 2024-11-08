cb8b4269faadbf0b443629f1ecb2e9009593445d
setavenger <github@snblago.com>
2024-11-08 21:37:02 +0100

    added info endpoint, closes #24

--- src/server/api.go
+++ src/server/api.go
@@ -19,8 +19,32 @@ import (
 //	Will keep for now just in case, so I don't have to refactor twice
 type ApiHandler struct{}
 
-type TxRequest struct {
-	Data string `form:"data" json:"data" binding:"required"`
+type Info struct {
+	Network                        string `json:"network"`
+	Height                         uint32 `json:"height"`
+	TweaksOnly                     bool   `json:"tweaks_only"`
+	TweaksFullBasic                bool   `json:"tweaks_full_basic"`
+	TweaksFullWithDustFilter       bool   `json:"tweaks_full_with_dust_filter"`
+	TweaksCutThroughWithDustFilter bool   `json:"tweaks_cut_through_with_dust_filter"`
+}
+
+func (h *ApiHandler) GetInfo(c *gin.Context) {
+	lastHeader, err := dblevel.FetchHighestBlockHeaderInvByFlag(true)
+	if err != nil {
+		common.ErrorLogger.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "could could not retrieve data from database",
+		})
+		return
+	}
+	c.JSON(http.StatusOK, Info{
+		Network:                        common.ChainToString(common.Chain),
+		Height:                         lastHeader.Height,
+		TweaksOnly:                     common.TweaksOnly,
+		TweaksFullBasic:                common.TweakIndexFullNoDust,
+		TweaksFullWithDustFilter:       common.TweakIndexFullIncludingDust,
+		TweaksCutThroughWithDustFilter: common.TweaksCutThroughWithDust,
+	})
 }
 
 func (h *ApiHandler) GetBestBlockHeight(c *gin.Context) {
@@ -301,6 +325,10 @@ func (h *ApiHandler) GetSpentOutpointsIndex(c *gin.Context) {
 	result.Data = resultData
 
 	c.JSON(http.StatusOK, result)
+}
+
+type TxRequest struct {
+	Data string `form:"data" json:"data" binding:"required"`
 }
 
 func (h *ApiHandler) ForwardRawTX(c *gin.Context) {
